d83c91edd130ed6baedd8f824faaf7b77ba05388
AngelloCruzYarleque <angellocruz0911@gmail.com>
2022-06-03 19:50:41 -0500

    cambio 2

--- app/src/main/java/com/usil/prueba_git/MainActivity.java
+++ app/src/main/java/com/usil/prueba_git/MainActivity.java
@@ -3,12 +3,19 @@ package com.usil.prueba_git;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView prueba;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        asignarReferencias();
+    }
+
+    private void asignarReferencias() {
+        prueba=findViewById(R.id.txtPrueba);
+        prueba.setText("Holaasd");
     }
 }
